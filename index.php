265e194c8a2cb3b0502fc78e0100e1c9fd9ecd04
Striladen <striladen@gmail.com>
2021-04-18 15:52:16 +0200

    Media Queries and SVG Background

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-$i = 1;
-?>
+<html>
+
+<head>
+    <title>Simple Personal Booker</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="box">
+            <h1>Simple Personal Booker</h1>
+            <p>A simple way to book a meeting.</p>
+        </div>
+    </div>
+</body>
+
+</html>
