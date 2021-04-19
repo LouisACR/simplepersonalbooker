5cea0cece5a7931a8bec1d81bb2411bc6303c415
Striladen <striladen@gmail.com>
2021-04-19 16:16:21 +0200

    fixed mobile responsiveness

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <html>
+
 <head>
     <title>Simple Personal Booker</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik">
     <link href="css/style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
+
 <body>
     <div class="container">
-        <div class="box">
-            <div class="box-header"></div>
-            <div class="row">
-                <div class="col-8-center">
-                    <h1>You have been invited.</h1>
-                </div>
-                <div class="col-12">
-                    <p>A simple way to book a meeting with your friends or colleagues.</p>
+        <div class="box"> <!-- Box START -->
+            <div class="box-header">
+                <div class="avatar">
+                    <img src="https://feedback.upvoty.com/images/avatar/39263/04f7f73267502fa37faa/">
+                    <h2>John A. SMITH</h2>
                 </div>
             </div>
-        </div>
+            <div class="box-content">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>You have been invited.</h1>
+                    </div>
+                    <div class="col-12">
+                        <p>A simple way to book a meeting with your friends or colleagues.</p>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- Box END -->
     </div>
 </body>
 
