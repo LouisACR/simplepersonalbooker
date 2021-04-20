d1c11991628ac9560e1d5f80bd782e2468aef269
Striladen <striladen@gmail.com>
2021-04-20 16:03:34 +0200

    calendar design finished + form input

--- global.php
+++ global.php
@@ -19,13 +19,20 @@
             <div class="box-content">
                 <div class="row">
                     <div class="col-12">
-                        <h1>You have been invited.</h1>
+                        <h1>Book your meeting now.</h1>
                     </div>
                     <div class="col-12">
-                        <p><b>John A. SMITH</b> has invited you (Nia ROMAIN) to join a meeting : <br><b class="text-blue">Business Virtual Classroom</b>.<br> Please select a slot to confirm this invitation.</p>
+                        <p>You can book a date to join <b>John A. SMITH</b> in a meeting: <br><b class="text-blue">Business Virtual Classroom</b>.
+                        <br> Please select a slot to confirm this meeting.</p>
                     </div>
                     <div class="col-12">
                         <h2 class="small-title">Book your slot now</h2>
+                    </div>
+                    <div class="col-12">
+                        <p class="pick">Please enter your email and choose a date and time.</p>
+                    </div>
+                    <div class="col-10-center">
+                        <input type="email" class="big-input" placeholder="Please enter your email">
                     </div>
                     <div class="calendar">
                         <h3 class="title">April 2021</h3>
@@ -44,29 +51,29 @@
                             <a href="javascript:void(0);" class="disabled">31</a>
                             <a href="javascript:void(0);">1</a>
                             <a href="javascript:void(0);">2</a>
-                            <a href="javascript:void(0);">3</a>
-                            <a href="javascript:void(0);">4</a>
+                            <a href="javascript:void(0);" class="disabled">3</a>
+                            <a href="javascript:void(0);" class="disabled">4</a>
                             <a href="javascript:void(0);">5</a>
                             <a href="javascript:void(0);">6</a>
-                            <a href="javascript:void(0);">7</a>
+                            <a class="selected" href="javascript:void(0);">7</a>
                             <a href="javascript:void(0);">8</a>
                             <a href="javascript:void(0);">9</a>
-                            <a href="javascript:void(0);">10</a>
-                            <a href="javascript:void(0);">11</a>
+                            <a href="javascript:void(0);" class="disabled">10</a>
+                            <a href="javascript:void(0);" class="disabled">11</a>
                             <a href="javascript:void(0);">12</a>
                             <a href="javascript:void(0);">13</a>
                             <a href="javascript:void(0);">14</a>
                             <a href="javascript:void(0);">15</a>
                             <a href="javascript:void(0);">16</a>
-                            <a href="javascript:void(0);">17</a>
-                            <a href="javascript:void(0);">18</a>
+                            <a href="javascript:void(0);" class="disabled">17</a>
+                            <a href="javascript:void(0);" class="disabled">18</a>
                             <a href="javascript:void(0);">19</a>
                             <a class="selected">20</a>
                             <a href="javascript:void(0);">21</a>
                             <a href="javascript:void(0);">22</a>
                             <a href="javascript:void(0);">23</a>
-                            <a href="javascript:void(0);">24</a>
-                            <a href="javascript:void(0);">25</a>
+                            <a href="javascript:void(0);" class="disabled">24</a>
+                            <a href="javascript:void(0);" class="disabled">25</a>
                             <a href="javascript:void(0);">26</a>
                             <a href="javascript:void(0);">27</a>
                             <a href="javascript:void(0);">28</a>
