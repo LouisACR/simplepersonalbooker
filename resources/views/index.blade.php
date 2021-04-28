31c03b183893b882daa8b4dc133ebd9de4eccd84
Striladen <striladen@gmail.com>
2021-04-28 16:31:56 +0200

    installed laravel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,6 @@
     <link href="css/style.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
-
 <body>
     <div class="container">
         <div class="box">
