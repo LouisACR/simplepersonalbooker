88aae171a27e2f043142f6db073f328259801e20
Striladen <striladen@gmail.com>
2021-07-05 14:44:48 +0200

    updated composer

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('booking/book', function (Request $request) {
+    return null;
+});
